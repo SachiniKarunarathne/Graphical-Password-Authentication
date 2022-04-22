ac354452cbd1dbdf962a500aec7abc34b915ebf9
SachiniKarunarathne <sachnissansala@gmail.com>
2022-04-23 01:25:50 +0530

    first commit

---
+++ gpas_final.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 20, 2019 at 08:47 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gpas_final`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `username` varchar(80) NOT NULL,
+  `password` varchar(80) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `phone` bigint(10) NOT NULL,
+  `userimage` varchar(800) NOT NULL,
+  `image1` varchar(800) NOT NULL,
+  `slice1` int(1) NOT NULL,
+  `image2` varchar(800) NOT NULL,
+  `slice2` int(1) NOT NULL,
+  `image3` varchar(800) NOT NULL,
+  `slice3` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `user` (`username`, `password`, `name`, `email`, `phone`, `userimage`, `image1`, `slice1`, `image2`, `slice2`, `image3`, `slice3`) VALUES
+('Thani', '81dc9bdb52d04dc20036dbd8313ed055', 'thanisthani', 'Rajkumar@gmail.com', 1233455661, '../user/images/user/Thani.png', 'http://localhost/Graphical-Password-Authentication-System-master/images/pw/image2.jpg', 3, 'http://localhost/Graphical-Password-Authentication-System-master/images/pw/image3.jpg', 3, 'http://localhost/Graphical-Password-Authentication-System-master/images/pw/image5.jpg', 3),
+('Thanis', '0ed2bfb6b5aa7552342b7b737104f76d', 'thanisthani', 'thanithanis171198@gmail.com', 773736352, '../user/images/user/default.png', 'http://localhost/Graphical-Password-Authentication-System-master/images/pw/image1.jpg', 3, 'http://localhost/Graphical-Password-Authentication-System-master/images/pw/image3.jpg', 3, 'http://localhost/Graphical-Password-Authentication-System-master/images/pw/image5.jpg', 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`,`email`,`phone`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
